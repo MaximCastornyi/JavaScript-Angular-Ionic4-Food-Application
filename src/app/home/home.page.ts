d36f3d9b0b9b07c1fad55259b301fea9a662ccf0
MaximCastornyi <nets.devs@gmail.com>
2020-01-28 11:32:11 +0200

    Ionic App

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MealdbApiService } from '../service/mealdb-api.service';
+import { take } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage {
+  meals$ = this.mealdb.meals$;
+
+  constructor(private mealdb: MealdbApiService) {
+      this.loadData();
+  }
+
+  loadData($event?) {
+      this.mealdb
+          .getWhatToEat()
+          .pipe(take(1))
+          .subscribe(done => {
+              if ($event) {
+                  $event.target.complete();
+              }
+          });
+  }
+}
